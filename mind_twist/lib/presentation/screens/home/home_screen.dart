45d1a9b40450307dca5833e734f21dfc98247de0
Naol Kumela <j45674190@gmail.com>
2024-05-31 15:39:39 +0300

    added the code to make functional the daily teaser button by picking randomly from the available categories

--- mind_twist/lib/presentation/screens/home/home_screen.dart
+++ mind_twist/lib/presentation/screens/home/home_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'dart:math';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,6 +13,16 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   int _selectedIndex = 0;
+  final List<String> categories = [
+    "General Knowledge",
+    "Science",
+    "History",
+    "Sports",
+    "Entertainment",
+    "Geography",
+  ];
+
+  Map<String, List<Map<String, dynamic>>> categoryQuestions = {};
 
   void _onItemTapped(int index) {
     setState(() {
@@ -22,11 +35,63 @@ class _HomeScreenState extends State<HomeScreen> {
           context.go('/teaser');
           break;
         case 2:
+          context.go('/analytics');
+          break;
+        case 3:
           context.go('/profile');
           break;
         default:
       }
     });
+  }
+
+  Future<void> _fetchTeasers() async {
+    try {
+      final response =
+          await http.get(Uri.parse('http://192.168.42.1:3000/api/teasers'));
+      if (response.statusCode == 200) {
+        final List<dynamic> teaserData = jsonDecode(response.body);
+
+        // Group teasers by category
+        setState(() {
+          categoryQuestions = teaserData.fold({}, (previousValue, element) {
+            final category = element['category'];
+            previousValue[category] = previousValue[category] ?? [];
+            previousValue[category]!.add(element);
+            return previousValue;
+          });
+        });
+      } else {
+        print('Failed to load teasers');
+      }
+    } catch (error) {
+      print('Error fetching teasers: $error');
+    }
+  }
+
+  Future<void> _navigateToRandomTeaser() async {
+    // Fetch teasers if not already fetched
+    if (categoryQuestions.isEmpty) {
+      await _fetchTeasers();
+    }
+
+    // Choose a random category
+    final random = Random();
+    final randomCategoryIndex = random.nextInt(categories.length);
+    final selectedCategory = categories[randomCategoryIndex];
+
+    // Navigate to the TeaseScreen with the selected category
+    context.push('/tease_screen', extra: {
+      'categoryName': selectedCategory,
+      'questions': categoryQuestions[selectedCategory]!,
+      'currentQuestionIndex': 0,
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchTeasers();
   }
 
   @override
@@ -47,7 +112,7 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
             const SizedBox(height: 10),
             ElevatedButton(
-              onPressed: () {},
+              onPressed: _navigateToRandomTeaser,
               child: const Text(
                 'Daily Twist',
                 style: TextStyle(
